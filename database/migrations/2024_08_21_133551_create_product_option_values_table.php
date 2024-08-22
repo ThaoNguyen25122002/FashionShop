496209519bbd161b01da30f92af6ad98b15ca4b8
Cong Thao <nlcongthao@gmail.com>
2024-08-22 20:23:56 +0700

    migrate refresh

--- database/migrations/2024_08_21_133551_create_product_option_values_table.php
+++ database/migrations/2024_08_21_133551_create_product_option_values_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('product_option_values', function (Blueprint $table) {
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
-            $table->foreignId('color_id')->constrained()->onDelete('cascade');
-            $table->foreignId('size_id')->constrained()->onDelete('cascade');
+            $table->foreignId('color_id')->constrained('option_values')->onDelete('cascade');
+            $table->foreignId('size_id')->constrained('option_values')->onDelete('cascade');
             $table->primary(['product_id', 'size_id', 'color_id']);
             $table->unsignedBigInteger('quantity');
+            // Tạo unique để tránh trùng lặp biến thể
+            $table->unique(['product_id', 'color_id', 'size_id']);
+
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,29 +17,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(1)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test1@example.com',
-        //     'role' => 'admin'
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test1@example.com',
+            'role' => 'admin'
+        ]);
         // Category::factory()->create([
         //     'name' => 'Áo Nam',
         //     'sort' => 1,
         //     'role' => true
         // ]);
-        $categories
-        = [
-                   ['name' => 'Áo sơ mi', 'sort' => 1, 'is_show' => 1],
-                   ['name' => 'Áo thun', 'sort' => 2, 'is_show' => 1],
-                   ['name' => 'Áo len', 'sort' => 3, 'is_show' => 1],
-                   ['name' => 'Áo khoác', 'sort' => 4, 'is_show' => 1],
-                   ['name' => 'Áo vest', 'sort' => 5, 'is_show' => 1],
-                   ['name' => 'Quần jeans', 'sort' => 6, 'is_show' => 1],
-                   ['name' => 'Quần tây', 'sort' => 7, 'is_show' => 1],
-                   ['name' => 'Quần short', 'sort' => 8, 'is_show' => 1],
-                   ['name' => 'Quần jogger', 'sort' => 9, 'is_show' => 1],
-                   ['name' => 'Đầm maxi', 'sort' => 10, 'is_show' => 1],
-               ];
-               Category::insert($categories);
+        // $categories
+        // = [
+        //            ['name' => 'Áo sơ mi', 'sort' => 1, 'is_show' => 1],
+        //            ['name' => 'Áo thun', 'sort' => 2, 'is_show' => 1],
+        //            ['name' => 'Áo len', 'sort' => 3, 'is_show' => 1],
+        //            ['name' => 'Áo khoác', 'sort' => 4, 'is_show' => 1],
+        //            ['name' => 'Áo vest', 'sort' => 5, 'is_show' => 1],
+        //            ['name' => 'Quần jeans', 'sort' => 6, 'is_show' => 1],
+        //            ['name' => 'Quần tây', 'sort' => 7, 'is_show' => 1],
+        //            ['name' => 'Quần short', 'sort' => 8, 'is_show' => 1],
+        //            ['name' => 'Quần jogger', 'sort' => 9, 'is_show' => 1],
+        //            ['name' => 'Đầm maxi', 'sort' => 10, 'is_show' => 1],
+        //        ];
+        //        Category::insert($categories);
     }
 }
